dcd4b941cab47cb04cd4f9f9b80c343124e97b64
ADHI OFC <aadhiaadhi598@gmail.com>
2024-12-06 14:32:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process .env.ALIVE_IMG ||"https://pomf2.lain.la/f/2ad8ap67.jpg",
+ALIVE_MSG: process .env.ALIVE_MSG ||"HELLOW,QUEEN MINUKI MD IS ALIVE NOW",
 };
