3f1c219dad397728f4edeedeed26462bf76de933
My-text-Git <webmy5404@gmail.com>
2024-12-08 17:52:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID||"vFZh2ZxQ#EdqnzFjBLiAMYZShYF56IRZHUISlcFKWejnu1qvaW8A",
 ALIVE_IMG: process .env.ALIVE_IMG ||"https://pomf2.lain.la/f/2ad8ap67.jpg",
 ALIVE_MSG: process .env.ALIVE_MSG ||"HELLOW,QUEEN MINUKI MD IS ALIVE NOW",
 };
